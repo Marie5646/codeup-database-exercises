362949d0bfc0a01d75aa32c3d2b846bd52215ab6
Asia Autrey <asia.autrey0@gmail.com>
2023-05-09 12:26:02 -0500

    exercise tables.spl

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        song_name CHAR(50),
+                        artist  CHAR(50) NOT NULL,
+                        release_date  INT(4) DATE NOT NULL ,
+                        sales  FLOAT(10) NOT NULL,
+                        genre CHAR(50) NOT NULL,
+                        PRIMARY KEY (id)
+);
